32b4664f10dfdbc6a011d459f2b636ea27d20d30
FormulaCarbon <siddhuk.learning@gmail.com>
2024-07-25 17:24:12 -0500

    oopsie

--- src/terminalrunner.py
+++ src/terminalrunner.py
@@ -5,7 +5,5 @@ objpath = input("Path to .obj file: ")
 compartment = input("Compartment name: ")
 faction = input("Faction: ")
 vehicle = input("Vehicle: ")
-
-time.sleep(5)
-
 print(importer(objpath, compartment, asVehicle = f"{faction}.{vehicle}")[0])
+time.sleep(5)
